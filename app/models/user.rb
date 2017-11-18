2b068b3df2e3d51bf717a65d8850151e8d5bd93c
David Allen <dallen@zendesk.com>
2017-11-18 17:29:35 +1100

    Add clearance user auth gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  include Clearance::User
+
   has_many :posts
   validates :name, presence: true, length: { maximum: 100 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171112000055) do
+ActiveRecord::Schema.define(version: 20171118061925) do
 
   create_table "posts", force: :cascade do |t|
     t.integer "user_id"
@@ -23,6 +23,12 @@ ActiveRecord::Schema.define(version: 20171112000055) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
+    t.string "encrypted_password", limit: 128
+    t.string "confirmation_token", limit: 128
+    t.string "remember_token", limit: 128
+    t.index ["email"], name: "index_users_on_email"
+    t.index ["remember_token"], name: "index_users_on_remember_token"
   end
 
 end
