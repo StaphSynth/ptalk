5abf5855a2129a1b4c72185add5e3856a47d20e7
David Allen <dallen@zendesk.com>
2017-12-04 09:09:47 +1100

    Switch to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20171123052340) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "channels", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
